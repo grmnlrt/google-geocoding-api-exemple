f11c75af177ea2e459702872b88366b633239fa3
Germain Loret <germain.loret@gmail.com>
2018-03-12 19:08:48 +0100

    done

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
-console.log("Hello from src/browser.js");
+const form = document.querySelector('#geocode');
+const address = document.querySelector('#address');
+const coordinates = document.querySelector('#coordinates');
+
+if (form) {
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address.value}***&key=AIzaSyDhAHbgc4cJH48P6ZQJsCHORGCKLQ5Kd6Y`)
+      .then(response => response.json())
+      .then((data) => {
+        data.results.forEach((result) => {
+          coordinates.insertAdjacentHTML('beforeend', `
+            <h3>${result.formatted_address}</h3>
+            <p><strong>Lat:</strong> ${result.geometry.location.lat} / <strong>Lng:</strong> ${result.geometry.location.lng}</p>
+          `);
+        });
+      });
+  });
+}
