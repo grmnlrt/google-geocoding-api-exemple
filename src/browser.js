2a9343fca3403bcdf070db00271a68e0cd791b8d
Germain Loret <germain.loret@gmail.com>
2018-03-13 09:31:04 +0100

    update

--- src/browser.js
+++ src/browser.js
@@ -5,9 +5,11 @@ const coordinates = document.querySelector('#coordinates');
 if (form) {
   form.addEventListener('submit', (e) => {
     e.preventDefault();
+
     fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address.value}***&key=AIzaSyDhAHbgc4cJH48P6ZQJsCHORGCKLQ5Kd6Y`)
       .then(response => response.json())
       .then((data) => {
+        coordinates.innerHTML = '';
         data.results.forEach((result) => {
           coordinates.insertAdjacentHTML('beforeend', `
             <h3>${result.formatted_address}</h3>
